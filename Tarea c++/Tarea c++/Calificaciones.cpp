5623d9d69fcb1b93ebb97ca66f16b9a44c42d56a
Miguel <anig1404@gmail.com>
2025-03-06 00:35:05 -0600

    Avance en el programa de calificaciones

--- Tarea c++/Tarea c++/Calificaciones.cpp
+++ Tarea c++/Tarea c++/Calificaciones.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+
+	cout << "Ingrese la cantidad de alumnos para evaluar: " << endl;
+	cin >> n;
+
+	vector<int> arr(n);
+
+	for (int i = 1; i <= n; i++) {
+		cout << i << endl;
+	}
+
+	return 0;
+}
