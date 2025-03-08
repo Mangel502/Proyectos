333939b7d4c0d84639ef204c371d6ede410c7975
Miguel <anig1404@gmail.com>
2025-03-07 23:33:19 -0600

    Avance en calificaciones

--- Tarea c++/Tarea c++/Calificaciones.cpp
+++ Tarea c++/Tarea c++/Calificaciones.cpp
@@ -4,16 +4,37 @@
 using namespace std;
 
 int main() {
-	int n;
+	int n, suma = 0;
+	float promedio;
 
 	cout << "Ingrese la cantidad de alumnos para evaluar: " << endl;
 	cin >> n;
+	cout << endl;
+	vector<int> cal(n);
 
-	vector<int> arr(n);
-
-	for (int i = 1; i <= n; i++) {
-		cout << i << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Ingrese la calificacion del alumno " << i + 1 << ": ";
+		cin >> cal[i];
 	}
+	cout << endl;
+	cout << "Las calificaiones son: " << endl;
+	for (int i = 0; i < n;i++) {
+		cout << "Alumno " << i + 1 << ": " << cal[i] << endl;
+	}
+	cout << endl;
+
+
+
+	for (int i = 0; i < n; i++) {
+		suma += cal[i];
+	}
+	promedio = static_cast<float>(suma) / n;
+	cout << "El promedio es igual a :" << promedio;
+
+	for (int i = 0; i < n; i++) {
+		;
+	}
+
 
 	return 0;
 }
